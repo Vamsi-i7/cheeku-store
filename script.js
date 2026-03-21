17cc91711e1440de933a0f7a498eb4c7678cb4be
BALA VAMSI KRISHNA <vaniramesh3484@gmail.com>
2026-03-21 20:30:23 +0530

    completed API integration with product display and basic UI styling

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 const API_URL = "https://fakestoreapi.com/products";
 
+const productsContainer = document.getElementById("products");
+const loading = document.getElementById("loading");
+
 async function fetchProducts() {
     try {
-        const res = await fetch(API_URL);  //Calls API
-        const data = await res.json();  //Converts response to json
+        const res = await fetch(API_URL);
+        const data = await res.json();
 
-        console.log(data); // check data first
+        loading.style.display = "none";
+
+        displayProducts(data);
+
     } catch (error) {
-        console.log("Error fetching data:", error);  //Error handling
+        console.log("Error fetching data:", error);
     }
+}
+
+function displayProducts(products) {
+    productsContainer.innerHTML = products.map(product => `
+        <div class="card">
+            <img src="${product.image}" width="100">
+            <h3>${product.title}</h3>
+            <p>₹${product.price}</p>
+        </div>
+    `).join("");
 }
 
 fetchProducts();
